03d2db8cdde35cbadcb1264466ca9cc870e2f8d4
JEBA FAWJIA <80630408+jeba-tech@users.noreply.github.com>
2021-11-14 01:20:50 +0600

    Add files via upload

---
+++ login.js
@@ -0,0 +1,21 @@
+
+var modal = document.getElementById('login-form');
+window.onclick = function(event) 
+{
+    if (event.target == modal) 
+    {
+        modal.style.display = "none";
+    }
+}
+
+
+
+var modal = document.getElementById('signup-form');
+window.onclick = function(event) 
+{
+if (event.target == modal) 
+{
+  modal.style.display = "none";
+}
+}
+
